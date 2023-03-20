0594870ecb4e97be85eb27ba739596024d973b63
zen0no <dolgovyazovandrej@gmail.com>
2023-03-20 10:04:10 +0200

    Initial commit

---
+++ semantic_classifier/model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+
+class LSTM(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
