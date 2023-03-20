ece4adbf1a99f184cbf73ac45ab80d951cd90334
zen0no <dolgovyazovandrej@gmail.com>
2023-03-20 14:41:31 +0200

    Add LSTM Review Classifier

--- semantic_classifier/model.py
+++ semantic_classifier/model.py
@@ -4,9 +4,18 @@ import torch.nn.functional as F
 
 
 
-class LSTM(nn.Module):
-    def __init__(self):
+class ReviewClassifier(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_size):
         super().__init__()
 
-    def forward(self, x):
-        pass
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.encoder = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, num_layers=1)
+        self.predictor = nn.Linear(hidden_size, 10)
+
+    def forward(self, seq):
+        out, (hidden, _) = self.encoder(self.embedding(seq))
+        preds = self.predictor = self.predictor(hidden.squeeze(0))
+
+        return F.softmax(preds)
+    
+    
