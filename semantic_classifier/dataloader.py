27d694c6ad10ecb4c45ad33edb70f8828f3e841f
zen0no <dolgovyazovandrej@gmail.com>
2023-03-21 12:12:54 +0200

    Add BatchSampler and collate_batch

--- semantic_classifier/dataloader.py
+++ semantic_classifier/dataloader.py
@@ -1,11 +1,16 @@
 from torch.utils.data import Dataset
+from torch.utils.data import Sampler
+from torch.nn.utils.rnn import pad_sequence
 
+import torch.nn.functional as F
+import torch
 
 from spacy.lang.en import English
 
 import os
 from typing import Tuple
 
+import random
 
 
 class IMDBReviewDataset(Dataset):
@@ -33,8 +38,53 @@ class IMDBReviewDataset(Dataset):
             text = f.read()
 
             tokens = [token.text for token in self.tokenizer(text)]
-            rating = int(item.split('.')[0].split('_')[-1])
+            rating = item.split('.')[0].split('_')[-1]
 
-            return (tokens, rating, self.tokenizer(text))
+            return (rating, tokens)
 
+
+
+class BatchSamplerSimilarLength(Sampler):
+    def __init__(self, dataset, batch_size, indicies=None, shuffle=True):
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+        # indicies and lengths
+        self.indicies = [(i, len(s[1])) for i, s in enumerate(dataset)]
+
+        if indicies is None:
+            self.indicies = torch.tensor(self.indicies)[indicies].tolist()
+
+    def __iter__(self):
+        if self.shuffle:
+            random.shuffle(self.indicies)
+        
+        pooled_indices = []
+
+        for i in range(0, len(self.indicies), self.batch_size * 100):
+            pooled_indices.extend(sorted(self.indicies[i:i + self.batch_size * 100], key=lambda x: x[1]))
+        self.pooled_indices = [x[0] for x in pooled_indices]
+
+        batches = [self.pooled_indices[i:i + self.batch_size] for i in range(0, len(self.pooled_indices), self.batch_size)]
+
+        if self.shuffle:
+            random.shuffle
+        for batch in batches:
+            yield batch
+
+        def __len__(self):
+            return len(self.pooled_indices) // self.batch_size
     
+
+
+def transform_label(label):
+    return F.one_hot(label, num_classes=10)
+
+def collate_batch(batch):
+    label_list, tokens_list = [], []
+
+    for (_label, _tokens) in batch:
+        label_list.append(transform_label(_label))
+        tokens_list.append(_tokens)
+    
+    return torch.tensor(label_list), pad_sequence(tokens_list)
