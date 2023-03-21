71330161fd071e0b091123067ea80fcc845bc05e
zen0no <dolgovyazovandrej@gmail.com>
2023-03-21 10:29:22 +0200

    Bugfix

--- semantic_classifier/dataloader.py
+++ semantic_classifier/dataloader.py
@@ -26,14 +26,15 @@ class IMDBReviewDataset(Dataset):
         return len(self._classes_entry_list)
     
 
-    def __getitem__(self, idx) -> Tuple():
+    def __getitem__(self, idx) -> Tuple:
         item = self._classes_entry_list[idx]
 
-        with open(os.path.join(self.root_dir, *item)) as f:
+        with open(os.path.join(self.root_dir, *item), encoding="utf-8") as f:
             text = f.read()
 
-            tokens = self.tokenizer(text)
+            tokens = [token.text for token in self.tokenizer(text)]
+            rating = int(item.split('.')[0].split('_')[-1])
 
-            return (tokens, self.tokenizer(text))
+            return (tokens, rating, self.tokenizer(text))
 
     
